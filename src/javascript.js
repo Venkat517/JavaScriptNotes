59bf214b20d80580e3e756349967da8905d144c2
Venkat <vbonam@kent.edu>
2021-03-13 15:00:45 +0530

    Added js notes

---
+++ src/javascript.js
@@ -0,0 +1,49 @@
+// JAVA SCRIPRT
+
+Mongo DB, Express, React, Node - All three utilizes javascript
+ 
+Version: Both js and ts belongs to ES
+    ES - 
+
+dataType: Frist thing to check in any language
+
+    String : ''  or " " (any thing in a quote is String)
+    Number : 23, 23.232, 232422424
+    Boolean : true or false 
+
+    null
+    undefined
+
+variable: 
+
+defing a variable: 
+    var a = 'ds'
+    var b = 2244
+    var c = true
+Typeof is inbuilt method in the javascript which tells the typeof variable
+
+modules of smaller to larger is always smaller
+
+2%3 = 2
+
+6%7 = 6
+
+if we add + with string it with Concate 
+String + string = String
+number + number = number
+string + number = string
+number + string = string 
+
+'10'+10+'10'
+10+10+'10'
+'10'+10+10
+
+
+'10'+10+'10'-1
+10+10+'10'-1
+2009
+'10'+10+10-1
+101009
+
+
+
