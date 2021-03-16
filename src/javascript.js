81a2c434dbfb838ddcfe519c549357ab650579fa
Venkat <vbonam@kent.edu>
2021-03-16 15:30:06 +0530

    updated changes day2

--- src/javascript.js
+++ src/javascript.js
@@ -45,5 +45,46 @@ number + string = string
 '10'+10+10-1
 101009
 
+if both the side is a pure number as string or number with + they will Concate
+but with , * / it will do the operation
 
+true is eqaul to 1
+false is eqaul to 0
+
+true + true 
+2
+true + 10 
+11
+
+'hai'+ true
+'haitrue'
+
+// comparsion operation
+
+= Assingment
+== compare the data
+=== compare the data and dataType
+
+JavaScript is caseSensitive
+
+//checking the output and debugging
+
+ console.log('this is mydata')
+
+ alert('just for notification');
+
+ comfirm('do u want to leave?')
+ ok = true
+ cancel = false
+
+ prompt('to take the user input')
+ it will take the value which is Submitted by the user 
+ it will return as the string from input box or prompt or anything
+
+ 
+
+
+
+
+ 
 
