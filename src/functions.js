fc222f0cd2bfc0e778a89afd991174f286b122dc
Venkat517 <vbonam@kent.edu>
2022-04-24 21:19:03 +0530

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -2,6 +2,8 @@
 // Rest operator
 // we need to sum the tot items in the cart with discount, we use rest op as it will consider all arg as an array 
 
+we can have any no of parameters before the rest operator (a,b,c,..etc, ...x)
+
 function sum(discount, ...prices) {
     const total = prices.reduce((acc, price) => acc + price);
     return total * (1 - discount);
