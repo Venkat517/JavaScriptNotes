2d35d716b154abdb88d62fa3feed402f65b5e108
Venkat517 <vbonam@kent.edu>
2022-04-25 00:37:48 +0530

    Create Getter and setter.js

---
+++ src/Getter and setter.js
@@ -0,0 +1,20 @@
+// Getter - to get the access from the obj
+
+const person = {
+  firstName : 'Venkata S',
+  lastName : 'Bonam',
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`
+  },
+  set fullName(value) {
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
+};
+
+console.log(person.firstName);
+console.log(person.fullName);
+person.fullName = 'Venkata Satyanarayana Bonam'
+console.log(person.firstName);
+console.log(person.fullName);
