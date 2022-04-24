23a8573bfc56b108b9516e9630306b76b54b82fd
Venkat517 <vbonam@kent.edu>
2022-04-25 01:23:15 +0530

    Update Try and catch.js

--- src/Try and catch.js
+++ src/Try and catch.js
@@ -1,1 +1,37 @@
 
+const person = {
+  firstName : 'Venkata S',
+  lastName : 'Bonam',
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`
+  },
+  set fullName(value) {
+    if (typeof value !== 'string')
+      throw new Error ('value is not a string')
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
+};
+
+console.log(person.firstName);
+console.log(person.fullName);
+
+try {
+person.fullName = null;
+}
+
+catch(e) {
+  console.log(e);
+}
+
+console.log(person.firstName);
+console.log(person.fullName);
+
+console.log(person);
+
+o/p
+
+Error: value is not a string
+    at Object.set fullName [as fullName] (<anonymous>:11:42)
+    at <anonymous>:22:19
