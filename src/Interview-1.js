be9b599147a8c18ddcf8e2e735890ae42fc93ac5
Venkat517 <vbonam@kent.edu>
2022-04-26 01:18:03 +0530

    Update Interview-1.js

--- src/Interview-1.js
+++ src/Interview-1.js
@@ -1,1 +1,21 @@
+// Difference between Var and Let
+
+Var - It is used in the javascript from the begning
+    - It is a function scope
+    - It get's hoisted
+
+let - It is introduced in the ES6/ES2015
+    - It is block scope(var dec using let will die within the block)
+    - It doesn't get hoisted
+
+// Difference between == and ===
+
+== - compare the value
+
+=== - compares both the value and its type
+
+// Difference between Let and Const
+
+
+
 
