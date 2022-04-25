802229ace45117c65707bad7680732baa853c857
Venkat517 <vbonam@kent.edu>
2022-04-25 12:51:09 +0530

    Update Oops pillar blocks.js

--- src/Oops pillar blocks.js
+++ src/Oops pillar blocks.js
@@ -1,7 +1,15 @@
 // The main pillar blocks of any opps was
 
 Abstraction
+  - we group related variable and functions together, hence we can reduce complexity and we can reuse them in differeny parts
+  (Reduce complexity + increase reuseability)
+
 inheritance
+  - we eliminates the redundant
 polymorphism
+  - we can refactor ugly switch case statements.
 enapsulation
+  - we hide the details and complexity and show only the essentials
+  (Reduce complexity + isolate the impact of changes)
+  
 
