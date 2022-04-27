44c693a600e4d66e376da099c58108444866e278
Venkat517 <vbonam@kent.edu>
2022-04-27 22:32:05 +0530

    Update Interview.js

--- src/Interview.js
+++ src/Interview.js
@@ -1,1 +1,23 @@
+// What is prototype in javascript //
+prototypes allows javascript objects to inherit from one and other
+
+eg:
+
+const vehicle = {
+	drive: function() {
+  	console.log('I am driving')
+  }
+}
+
+const car = {
+	make : 'Honda'
+}
+
+Object.setPrototypeOf(car,vehicle)
+car.drive()
+
+o/p
+I am driving
+
+
 
