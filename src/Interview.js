5371cb39d2ff0744c60f42a0c00e695b8bb6f5ca
Venkat517 <vbonam@kent.edu>
2022-04-28 21:33:51 +0530

    Update Interview.js

--- src/Interview.js
+++ src/Interview.js
@@ -1,3 +1,19 @@
+// Difference between Var and Let
+
+Var - It is used in the javascript from the begning
+    - It is a function scope
+    - It get's hoisted
+let - It is introduced in the ES6/ES2015
+    - It is block scope(var dec using let will die within the block)
+    - It doesn't get hoisted
+
+// Difference between == and ===
+
+== - compare the value
+
+=== - compares both the value and its type
+
+
 // What is prototype in javascript //
 prototypes allows javascript objects to inherit from one and other
 
